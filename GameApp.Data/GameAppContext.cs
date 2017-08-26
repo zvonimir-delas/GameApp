c3782d10bc5c88bd7a0cbf5f69ad6f566ba4e3aa
zvonimir-delas <6d.solin@gmail.com>
2017-08-26 16:19:10 +0200

    Added context and initializer files, no seed data

---
+++ GameApp.Data/GameAppContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using GameApp.Data.Models;
+
+namespace GameApp.Data
+{
+    public class GameAppContext : DbContext
+    {
+        public GameAppContext() : base()
+        {
+            Database.SetInitializer(new GameAppInitializer());
+        }
+
+        public virtual DbSet<Player> Players { get; set; }
+        public virtual DbSet<Match> Matches { get; set; }
+        public virtual DbSet<Team> teams { get; set; }
+        public virtual DbSet<Tournament> Tournaments { get; set; }
+    }
+}
---
+++ GameApp.Data/GameAppInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace GameApp.Data
+{
+    public class GameAppInitializer : DropCreateDatabaseIfModelChanges<GameAppContext>
+    {
+        protected override void Seed(GameAppContext context)
+        {
+            //SEED
+
+            base.Seed(context);
+        }
+    }
+}
